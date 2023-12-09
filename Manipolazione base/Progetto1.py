879431d66f516322f29c54e94511776e8802e338
Marco.Vivian <vivian.marco.eu@gmail.com>
2023-12-09 18:31:13 +0100

    last commit  09_12_23

--- Manipolazione base/Progetto1.py
+++ Manipolazione base/Progetto1.py
@@ -1,31 +1,46 @@
 #Marco Vivian 09/12/2023
-a=2
-a
-print("Questo progetto è pensato per riprendere Python dalle basi")
 
-b=3
-a
-a=4
-a
+
+print("First script")
+
 import pandas as pd
-a=5
-a
 import numpy as np
-a=6
-a
-#array
 
+#array as numbers
 x = np.array([1,2,3,4,5])
-x
-#y = np.array([[1,2],[3,4],[5,6]])
-#y
+print(x)
+print(x[0])
+#multidimensional array as list of list
+y = np.array([[1,2],[3,4],[5,6]])
+print(y)
+print(y[0,1])
+
+#shape ci ritorna le dimensioni
+print(x.shape)
+print(y.shape)
+
+print(y.shape[0],y.shape[1])
+
+print(len(y.shape))
 
 
+print(y)
+# usiamo numero elementi e esce una sola riga
+print(y.reshape(6))
 
-#shape
+#-1 capisce da solo
+print(y.reshape(-1))
 
+#sistema array su 2 righe e 3 colonne
+print(y.reshape(2,3))
 
+#-1 capisce da solo il numero di colonne date le 2 righe definite
+print(y.reshape(2,-1))
 
+x2 = np.array([1,2,3,4,5,6])
+print(x2)
+
+print(x2.reshape(3,2))
 
 
 
