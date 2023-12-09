98069bda1e4a4fc11012032afb91cc7c701792a4
Marco.Vivian <vivian.marco.eu@gmail.com>
2023-12-09 17:21:26 +0100

    TEST3

--- Manipolazione base/Progetto1.py
+++ Manipolazione base/Progetto1.py
@@ -1,20 +1,20 @@
+#Marco Vivian 09/12/2023
 
 print("Questo progetto è pensato per riprendere Python dalle basi")
-a = 3
-print(a)
 
-a= a+5
-print(a)
+import pandas as pd
+import numpy as np
 
+#array
 
-
-pip install numpy
-
-
-
+x = np.array([1,2,3,4,5])
+x
+#y = np.array([[1,2],[3,4],[5,6]])
+#y
 
 
 
+#shape
 
 
 
